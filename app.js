45647cfdfbe43db115ad84dabaf0feb324143954
Edward Kim <Ed@Edwards-MacBook-Pro.local>
2018-05-24 15:41:59 +0900

    Pushing the updated JavaScript and dependencies

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express')
 	, path = require('path')
 	, logger = require('morgan')
-	, bodyParser = require('body-parser');
+	, bodyParser = require('body-parser')
+	, neo4j = require('neo4j-driver').v1;
 	
 var app = express();
 
@@ -13,6 +14,23 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456'));
+var session = driver.session();
+
+app.get('/', function(req, res){
+	session
+		.run() //Cypher query
+		.then(function(result){
+			result.records.forEach(function(record){
+				console.log(record);
+			});
+		}) //
+		.catch(function(err){
+			console.log(err);
+		});
+	res.send('It works partially');
+});
 
 app.listen(3000);
 console.log('Server started on Port#: 3000');
